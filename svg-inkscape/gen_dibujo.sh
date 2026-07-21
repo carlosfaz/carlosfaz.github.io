d6ab49f6e6828e68c144fa8685ce85f9aafac8a4
carlos <usuario@local.lan>
2026-07-20 21:09:43 -0600

    daily update
    daily update

--- svg-inkscape/gen_dibujo.sh
+++ svg-inkscape/gen_dibujo.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-# Definir la variable base (sin extensión)
 x="dibujo1"
-
-# Ejecutar pdflatex e Inkscape
-pdflatex "${x}.tex" && inkscape "${x}.pdf" --pdf-poppler --export-area-drawing --export-type=svg --export-filename="${x}.svg"
-
-# Verificar si los comandos anteriores fueron exitosos
-if [ $? -eq 0 ]; then
-    # Eliminar archivos intermedios si el proceso fue exitoso
-    rm -f "${x}.aux" "${x}.log" "${x}.pdf"
-    echo "Proceso completado y archivos limpiados: ${x}.aux, ${x}.log, ${x}.pdf"
-else
-    echo "Error en el proceso. Los archivos intermedios se han conservado para depuración."
-fi
+pdflatex -interaction=batchmode "${x}.tex" >/dev/null 2>&1 && inkscape "${x}.pdf" --pdf-poppler --export-area-drawing --export-type=svg --export-filename="${x}.svg" && rm -f "${x}.aux" "${x}.log" "${x}.pdf" && echo "Completado" || echo "Error"
